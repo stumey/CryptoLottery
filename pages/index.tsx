73ed29007c7e62b925deb2ed8cb3da4b02938c52
Stefan Tumey <stef@stefans-air.lan>
2022-12-30 18:31:18 -0500

    Building out landing page with ticket buying functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,45 @@
-import { 
+import {
   useAddress,
   useContract,
   useContractRead,
   useContractWrite,
   useMetamask,
   useDisconnect,
-  ConnectWallet, 
-} from '@thirdweb-dev/react'
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Header from '../components/Header'
-import Loading from '../components/Loading'
-import Login from '../components/Login'
+  ConnectWallet,
+} from "@thirdweb-dev/react";
+import type { NextPage } from "next";
+import { ethers } from "ethers";
+import Head from "next/head";
+import { useState } from "react";
+import Header from "../components/Header";
+import Loading from "../components/Loading";
+import Login from "../components/Login";
+import { currency } from "../constants";
 
 const Home: NextPage = () => {
   const address = useAddress();
+  const [quantity, setQuantity] = useState<number>(1);
   const { contract, isLoading } = useContract(
     process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS
   );
+  const { data: expiration } = useContractRead(
+    contract, "expiration"
+  );
+  const { data: remainingTickets } = useContractRead(
+    contract, "RemainingTickets"
+  );
+  const { data: currentWinningReward } = useContractRead(
+    contract, "CurrentWinningReward"
+  );
+  const { data: ticketPrice } = useContractRead(
+    contract, "ticketPrice"
+  );
+  const { data: ticketCommission } = useContractRead(
+    contract, "ticketCommission"
+  );
 
-  if (isLoading) return ( <Loading /> )
-  if (!address) return ( <Login /> )
+  if (isLoading) return <Loading />;
+  if (!address) return <Login />;
 
   console.log(address);
   return (
@@ -29,9 +48,122 @@ const Home: NextPage = () => {
         <title>Crypto Lottery Draw</title>
       </Head>
 
-      <Header />
-    </div>
-  )
-}
+      <div className="flex-1">
+        <Header />
 
-export default Home
+        {/* The Next Draw Box */}
+        <div
+          className="space-y-5 md:space-y-0 m-5 md:flex md:flex-row 
+      items-start justify-center md:space-x-5"
+        >
+          <div className="stats-container">
+            <h1 className="text-5xl text-white font-semibold text-center">
+              The Next Draw
+            </h1>
+            <div className="flex justify-between p-2 space-x-2">
+              <div className="stats">
+                <h2 className="text-sm">Total Pool</h2>
+                <p className="text-xl">
+                  {currentWinningReward && 
+                    ethers.utils.formatEther(
+                      currentWinningReward.toString()
+                  )}{" "}
+                  {currency}
+                </p>
+              </div>
+              <div className="stats">
+                <h2 className="text-sm">Tickets Remaining</h2>
+                <p className="text-xl">{remainingTickets?.toNumber()}</p>
+              </div>
+            </div>
+            {/* Countdown Timer */}
+            {/* ... */}
+          </div>
+
+          <div className="stats-container space-y-2">
+            <div className="stats-container">
+              <div className="flex justify-between items-center text-white pb-2">
+                <h2>Price per ticket</h2>
+                <p>
+                {ticketPrice && 
+                    ethers.utils.formatEther(
+                      ticketPrice.toString()
+                  )}{" "}
+                  {currency}
+                </p>
+              </div>
+
+              <div
+                className="flex text-white items-center 
+            space-x-2 bg-[#091B1B] border-[#004337] border p-4"
+              >
+                <p>TICKETS</p>
+                <input
+                  className="flex w-full bg-transparent text-right outline-none"
+                  type="number"
+                  min={1}
+                  max={10}
+                  value={quantity}
+                  onChange={(e) => setQuantity(Number(e.target.value))}
+                />
+              </div>
+
+              <div className="space-y-2 mt-5">
+                <div
+                  className="flex items-center justify-between 
+              text-emerald-300 text-sm italic font-extrabold"
+                >
+                  <p>Total cost of tickets</p>
+                  <p>
+                    {ticketPrice &&
+                      Number(
+                        ethers.utils.formatEther(ticketPrice?.toString())
+                      ) * quantity}{" "}
+                      {currency}
+                  </p>
+                </div>
+
+                <div
+                  className="flex items-center justify-between 
+              text-emerald-300 text-xs italic"
+                >
+                  <p>Service fees</p>
+                  <p>
+                  {ticketCommission && 
+                    ethers.utils.formatEther(
+                      ticketCommission.toString()
+                  )}{" "}
+                  {currency}
+                  </p>
+                </div>
+
+                <div
+                  className="flex items-center justify-between 
+              text-emerald-300 text-xs italic"
+                >
+                  <p>+ Network Fees</p>
+                  <p>TBC</p>
+                </div>
+              </div>
+
+              <button
+                disabled={expiration?.toString() < Date.now().toString() ||
+                remainingTickets?.toNumber() === 0}
+                className="mt-5 w-full bg-gradient-to-br from-orange-500 to-emerald-600 
+              px-10 py-5 rounded-md text-white shadow-xl disabled:from-gray-600 
+              disabled:to-gray-100 disabled:text-gray-100 disabled:cursor-not-allowed"
+              >
+                Buy tickets
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* The price per ticket Box */}
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
